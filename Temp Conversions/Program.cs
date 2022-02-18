54fed5042c0c0c2fd1dbf0bebd1dd2746d8f3f98
jacob gishwiller <sirbruce13@yahoo.com>
2022-02-18 11:36:44 -0700

    first commit

---
+++ Temp Conversions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Temp_Conversions
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
