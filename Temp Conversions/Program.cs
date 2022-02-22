bfbf41c494e48559a708860001e465d34c7aa3a0
4400112799 <4400112799@BT144-PROG22>
2022-02-22 14:36:42 -0700

    first update

--- Temp Conversions/Program.cs
+++ Temp Conversions/Program.cs
@@ -4,9 +4,18 @@ namespace Temp_Conversions
 {
    class Program
    {
-      static void Main(string[] args)
-      {
-         Console.WriteLine("Hello World!");
-      }
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+
+
+            double celsius = 36;
+            Console.WriteLine("Celsius:" + celsius);
+
+            fahrenheit = 9.0 / 5.0 * celsius + 32;
+            Console.WriteLine("Fahrenheit " + fahrenheit);
+
+            Console.ReadLine();
+        }
    }
 }
